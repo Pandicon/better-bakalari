24283ebc6a77d23ae9d2b83a1da284b28303f639
Pandicon <70060103+Pandicon@users.noreply.github.com>
2023-05-13 11:40:59 +0200

    Add a login request

--- src/rendering/windows/login.cpp
+++ src/rendering/windows/login.cpp
@@ -1,8 +1,15 @@
-﻿#include "../../application.h"
+﻿#define CPPHTTPLIB_OPENSSL_SUPPORT
+
+#include <httplib.h>
+#include <iostream>
+
+#include "../../application.h"
 #include "../../imgui_utils.h"
 
 void Application::render_login() {
     ImGui::Begin(u8"Přihlášení");
+    ImGui::Text("Server URL");
+    ImGui::InputTextWithHint("##server_url", "server url", &state.api_url);
     ImGui::Text("Username");
     ImGui::InputTextWithHint("##username", "username", &state.username);
     ImGui::Text("Password");
@@ -15,8 +22,28 @@ void Application::render_login() {
     ImGui::SameLine();
     if (ImGui::Button(state.show_password ? "Hide password" : "Show password")) state.show_password = !state.show_password;
     if (ImGui::Button(u8"Přihlásit se")) {
+        httplib::SSLClient cli(state.api_url);
+        cli.set_ca_cert_path("./resources/ca-bundle.crt");
+        cli.set_connection_timeout(0, 300000); // 300 milliseconds
+        cli.set_read_timeout(5, 0); // 5 seconds
+        cli.set_write_timeout(5, 0); // 5 seconds
+
+        auto res = cli.Post("/api/login", "client_id=ANDR&grant_type=password&username=" + state.username + "&password=" + state.password, "application/x-www-form-urlencoded");
+
+        if (res) {
+            std::cout << res << std::endl;
+            std::cout << "Status: " << res->status << std::endl;
+            std::cout << "Body: " << res->body << std::endl;
+        }
+        else {
+            auto err = res.error();
+            std::cout << "HTTP error: " << httplib::to_string(err) << std::endl;
+        }
+
+        /*
         state.password = "";
         state.username = "";
+        */
     }
     ImGui::End();
 }
