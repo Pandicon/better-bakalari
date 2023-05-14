50c23a6581dc5f2e62a87e1d9b43ef0cddac7ca2
Pandicon <70060103+Pandicon@users.noreply.github.com>
2023-05-14 12:21:09 +0200

    Include date and log level in logs

--- src/log.cpp
+++ src/log.cpp
@@ -5,7 +5,11 @@
 std::shared_ptr<spdlog::logger> Log::s_logger;
 
 void Log::Init() {
-	spdlog::set_pattern("%^[%T] %v%$"); // Coloured, [HH:MM:SS] message
+#ifdef _BA_DEBUG
+	spdlog::set_pattern("%^[%Y-%m-%d %T] [%l] %v (%s:%#, %!)%$"); // Coloured, [YYYY-MM-DD HH:MM:SS] [LEVEL] message (file:line, function)
+#else
+	spdlog::set_pattern("%^[%Y-%m-%d %T] [%l] %v%$"); // Coloured, [YYYY-MM-DD HH:MM:SS] [LEVEL] message
+#endif
 	s_logger = spdlog::stdout_color_mt("APP");
 	s_logger->set_level(spdlog::level::trace);
 }
