506c601e268761c2b057c5d50ef1fd5151602b38
Pandicon <70060103+Pandicon@users.noreply.github.com>
2023-05-14 11:44:31 +0200

    Remove the colon from the log messages

--- src/log.cpp
+++ src/log.cpp
@@ -5,7 +5,7 @@
 std::shared_ptr<spdlog::logger> Log::s_logger;
 
 void Log::Init() {
-	spdlog::set_pattern("%^[%T]: %v%$"); // Coloured, [HH:MM:SS]: message
+	spdlog::set_pattern("%^[%T] %v%$"); // Coloured, [HH:MM:SS] message
 	s_logger = spdlog::stdout_color_mt("APP");
 	s_logger->set_level(spdlog::level::trace);
 }
